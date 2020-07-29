92933427ab5892bad4683cff1fd7747197c16838
Shamis Shukoor <shamis657@gmail.com>
2020-07-29 14:13:47 +0200

    Update build_config.sh

--- .github/build_config.sh
+++ .github/build_config.sh
@@ -7,9 +7,9 @@ echo "version is $version"
 if [ $osType == "android" ]; then
 echo "its android"
 widget="<widget id=\"ch.qualitas.ebgs\" version=\"$version\" xmlns=\"http://www.w3.org/ns/widgets\" xmlns:cdv=\"http://cordova.apache.org/ns/1.0\">"
+sed -i "/.*widget id=.*/c $widget" config.xml
 else
 echo "its iOS"
 widget="<widget id=\"ch.qualitasag.ebgs\" version=\"$version\" xmlns=\"http://www.w3.org/ns/widgets\" xmlns:cdv=\"http://cordova.apache.org/ns/1.0\">"
+gsed -i "/.*widget id=.*/c $widget" config.xml
 fi
-# replace the widget line with the updated one
-sed -i '' '/.*widget id=.*/c \ test' config.xml
